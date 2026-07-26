4f4905d5f241a6962a9f2f301aa9dab465c0083f
Sagar Choudhari <aiqalab.tech@gmail.com>
2026-07-26 16:06:11 +0530

    Added user story reader module

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("Welcome to AI QA Lab 🚀")
-print("Building AI-powered Quality Engineering solutions")
-print("First project: AI Test Case Generator")
+# AI QA Lab - Step 1
+# Read user story from file
+
+file_path = "data/login_story.txt"
+
+with open(file_path, "r") as file:
+    user_story = file.read()
+
+print("AI QA Lab - Input User Story")
+print("------------------------------")
+print(user_story)
